84f191f6080e360f0821a889b4570ab57146089d
Özde Kaymaz <59960415+ozdekymz@users.noreply.github.com>
2025-04-05 19:16:47 +0300

    MyFirstAutomatedProject
    src

---
+++ src3/test/java/Gender_Selection_Test.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class Gender_Selection_Test extends BaseTest{
+    @Test
+    public void check_male_radio_button(){
+
+    }
+    @Test
+    public void check_female_radio_button(){
+
+    }
+    @Test
+    public void check_other_radio_button(){
+
+    }
+
+}
